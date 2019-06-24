257ec63f4ca1766b2b15f010f3af42534140b487
Natália Luz <natalia_m_luz@estudante.sc.senai.br>
2019-06-24 16:34:29 -0300

    coloquei atributos dentro da função

--- mod4/1/todo.js
+++ mod4/1/todo.js
@@ -1,3 +1,7 @@
 function checaIdade(idade) {
-    
+    return new Promise(function(resolve, rejeita) {
+        setTimeout(function() {
+          return idade >= 18 ? resolve() : rejeita();
+        }, 2000);
+      });
 }
