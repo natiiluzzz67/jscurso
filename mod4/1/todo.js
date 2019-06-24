b7810a19ccc98c38fe4ed783eb1b0f56b412489a
Natália Luz <natalia_m_luz@estudante.sc.senai.br>
2019-06-24 16:36:18 -0300

    colequei uma idade e dei 2 console.log

--- mod4/1/todo.js
+++ mod4/1/todo.js
@@ -5,3 +5,11 @@ function checaIdade(idade) {
         }, 2000);
       });
 }
+
+checaIdade(20)
+.then(function() {
+console.log("Maior que 18");
+})
+.catch(function() {
+console.log("Menor que 18");
+});
