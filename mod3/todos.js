6157a28d9e6fc4d4c92ee75b70114cab283e9ae0
Natália Luz <natalia_m_luz@estudante.sc.senai.br>
2019-06-24 16:03:32 -0300

    criei 3 variaveis

--- mod3/todos.js
+++ mod3/todos.js
@@ -6,9 +6,12 @@ var todos = JSON.parse(localStorage.getItem("list_todos")) || [];
 function renderTodos() {
     listElement.innerHTML = "";
     for (todo of todos){
+        var todoElement = document.createElement("li");
+        var todoText = document.createTextNode(todo);
+        var linkElement = document.createElement("a");
 
 
-        
+
     }
 
 }
