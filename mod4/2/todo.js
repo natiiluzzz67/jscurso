b84e6673523bb53d8bef36b4d7129b5c20c00a9b
Natália Luz <natalia_m_luz@estudante.sc.senai.br>
2019-06-24 16:51:51 -0300

    criei um for e já coloquei seus atributos

--- mod4/2/todo.js
+++ mod4/2/todo.js
@@ -2,7 +2,15 @@ var listElement = document.querySelector('ul');
 var inputElement = document.querySelector('input');
 
 function renderRepositorios(repositorios) {
+    for (repo of repositorios) {
+        const textElement = document.createTextNode(repo.name);
+        const liElement = document.createElement('li');
+        liElement.appendChild(textElement);
+        listElement.appendChild(liElement);
 
 
-    
+    }
+
+
+
 }
